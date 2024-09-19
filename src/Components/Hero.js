f10b8018f17c672b8e707ea1012b69633a18d12c
Abhishek Sadhwani <abhisheksadhwani23@gmail.com>
2024-09-19 15:04:22 +0530

    correction

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -14,7 +14,7 @@ const Hero = () => {
             <div className="flex flex-wrap justify-center">
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
-                        <motion.h1 variants={(heroTransition(0))} initial="hidden" animate="visible" className="text-5xl pb-16 lg:text-7xl lg:mt-16 font-thin tracking-tight">Abhishek Sadhwani</motion.h1>
+                        <motion.h1 variants={(heroTransition(0))} initial="hidden" animate="visible" className="text-4xl pb-16 lg:text-7xl lg:mt-16 font-thin tracking-tight">Abhishek Sadhwani</motion.h1>
                         <motion.h3 variants={(heroTransition(0.5))} initial="hidden" animate="visible" className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl text-thin text-fill text-transparent tracking-tight">Front-End Developer</motion.h3>
                         <motion.p variants={(heroTransition(1))} initial="hidden" animate="visible" className="my-2 font-light traking-tighter py-6 max-w-lg">{HERO_CONTENT}</motion.p>
                     </div>
