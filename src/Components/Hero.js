ef0ab8512d56328083de76a4286dad8e814d5e2c
Abhishek Sadhwani <abhisheksadhwani23@gmail.com>
2024-11-29 13:19:38 +0530

    Corrected name size

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -13,8 +13,8 @@ export const Hero = () => {
             {/* <div className="flex flex-wrap justify-center"> */}
                 <div className="text-center">
                     <div className="flex flex-col items-center">
-                        <motion.h1 variants={(heroTransition(0))} initial="hidden" animate="visible" className="text-4xl pb-4 lg:text-7xl text-neutral-800 lg:mt-16 font-medium tracking-tight dark:text-neutral-300">Abhishek Sadhwani</motion.h1>
-                        <motion.h3 variants={(heroTransition(0.5))} initial="hidden" animate="visible" className="bg-gradient-to-r from-neutral-900 via-slate-600 to-neutral-500 bg-clip-text text-2xl lg:text-3xl text-thin text-fill text-transparent tracking-tight dark:from-pink-300 dark:via-slate-500 dark:to-purple-500">Front-End Developer</motion.h3>
+                        <motion.h1 variants={(heroTransition(0))} initial="hidden" animate="visible" className="text-3xl md:text-4xl lg:text-7xl pb-4 text-neutral-800 lg:mt-16 font-medium tracking-tight dark:text-neutral-300">Abhishek Sadhwani</motion.h1>
+                        <motion.h3 variants={(heroTransition(0.5))} initial="hidden" animate="visible" className="bg-gradient-to-r from-neutral-900 via-slate-600 to-neutral-500 bg-clip-text text-xl lg:text-3xl text-thin text-fill text-transparent tracking-tight dark:from-pink-300 dark:via-slate-500 dark:to-purple-500">Front-End Developer</motion.h3>
                         <motion.p variants={(heroTransition(1))} initial="hidden" animate="visible" className="my-2 text-neutral-900 traking-tighter py-10 max-w-lg dark:text-neutral-300">{HERO_CONTENT}</motion.p>
                     </div>
                 </div>           
