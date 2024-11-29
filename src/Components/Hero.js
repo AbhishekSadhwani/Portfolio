b2ce91d03656996b0df19d6bbeb8692530415672
Abhishek Sadhwani <abhisheksadhwani23@gmail.com>
2024-11-29 14:12:57 +0530

    some changes

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -9,13 +9,13 @@ export const Hero = () => {
     });
 
     return (
-        <section className="border-b border-gray-200 dark:border-neutral-900 pb-4 lg:mb-36">
+        <section className="pb-4 lg:mb-36">
             {/* <div className="flex flex-wrap justify-center"> */}
                 <div className="text-center">
                     <div className="flex flex-col items-center">
                         <motion.h1 variants={(heroTransition(0))} initial="hidden" animate="visible" className="text-3xl md:text-4xl lg:text-7xl pb-4 text-neutral-800 lg:mt-16 font-medium tracking-tight dark:text-neutral-300">Abhishek Sadhwani</motion.h1>
                         <motion.h3 variants={(heroTransition(0.5))} initial="hidden" animate="visible" className="bg-gradient-to-r from-neutral-900 via-slate-600 to-neutral-500 bg-clip-text text-xl lg:text-3xl text-thin text-fill text-transparent tracking-tight dark:from-pink-300 dark:via-slate-500 dark:to-purple-500">Front-End Developer</motion.h3>
-                        <motion.p variants={(heroTransition(1))} initial="hidden" animate="visible" className="my-2 text-neutral-900 traking-tighter py-10 max-w-lg dark:text-neutral-300">{HERO_CONTENT}</motion.p>
+                        {/* <motion.p variants={(heroTransition(1))} initial="hidden" animate="visible" className="my-2 text-neutral-900 traking-tighter py-10 max-w-lg dark:text-neutral-300">{HERO_CONTENT}</motion.p> */}
                     </div>
                 </div>           
                 {/* <div className="w-full lg:w-1/2 lg:p-8">
