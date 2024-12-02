91b94b653d32aa374d936657e03c904178f01e57
Abhishek Sadhwani <abhisheksadhwani23@gmail.com>
2024-12-02 21:57:59 +0530

    Text jusitified and image position change

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,20 +4,20 @@ import { ABOUT_TEXT } from "../constants/index";
 
 export const About = () => {
     return (
-        <section className='border-b border-gray-200 dark:border-neutral-900 pb-4'>
+        <section className='border-b border-gray-200 text-center dark:border-neutral-900 pb-4'>
             <motion.h1 initial={{y:-100,opacity:0}} whileInView={{y:0,opacity:1}} viewport={{ once: true }} transition={{duration:1.5}} className='text-2xl md:text-4xl text-neutral-900 text-center my-20 dark:text-neutral-300'>About <span className="text-slate-700 dark:text-gray-500">Me</span></motion.h1>
-            <div className='flex flex-wrap'>
-                <div className='w-full lg:w-1/2 lg:p-8'>
+            {/* <div className='flex flex-wrap'> */}
+                {/* <div className='w-full lg:w-1/2 lg:p-8'>
                     <div className="w-3/4 mx-auto flex items-center justify-center">
                         <motion.img initial={{x:-100,opacity:0}} whileInView={{x:0,opacity:1}} viewport={{ once: true }} transition={{duration:1}} className="rounded-2xl" src={profile} alt="" />
                     </div>
-                </div>
-                <div className="w-full lg:w-1/2">
-                    <div className="flex justify-center lg:justify-start">
-                        <motion.p initial={{x:100,opacity:0}} viewport={{ once: true }} whileInView={{x:0,opacity:1}} transition={{duration:1}} className="text-justify text-base md:text-lg max-w-xl my-2 py-6 text-neutral-900 dark:text-neutral-300">{ABOUT_TEXT}</motion.p>
+                </div> */}
+                {/* <div className=""> */}
+                    <div className="">
+                        <motion.p initial={{x:100,opacity:0}} viewport={{ once: true }} whileInView={{x:0,opacity:1}} transition={{duration:1}} className="text-justify text-base md:text-lg max-w-xl mx-auto my-2 py-6 text-neutral-900 dark:text-neutral-300">{ABOUT_TEXT}</motion.p>
                     </div>
-                </div>
-            </div>
+                {/* </div> */}
+            {/* </div> */}
         </section>
     )
 };
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-
+import heroImage from "../assets/profile.png"
 
 export const Hero = () => {
     const heroTransition = (delay) => ({
@@ -9,20 +9,20 @@ export const Hero = () => {
 
     return (
         <section className="pb-4 lg:mb-36">
-            {/* <div className="flex flex-wrap justify-center"> */}
-                <div className="text-center">
+            <div className="flex flex-col lg:flex-row gap-10 justify-center">
+                <div className="w-full lg:w-1/2 lg:p-8 text-center">
                     <div className="flex flex-col items-center">
                         <motion.h1 variants={(heroTransition(0))} initial="hidden" animate="visible" className="text-3xl md:text-6xl lg:text-7xl pb-4 text-neutral-800 lg:mt-16 font-medium tracking-tight dark:text-neutral-300">Abhishek Sadhwani</motion.h1>
                         <motion.h3 variants={(heroTransition(0.5))} initial="hidden" animate="visible" className="bg-gradient-to-r from-neutral-900 via-slate-600 to-neutral-500 bg-clip-text text-xl md:text-2xl lg:text-4xl text-thin text-fill text-transparent tracking-tight dark:from-pink-300 dark:via-slate-500 dark:to-purple-500">Front-End Developer</motion.h3>
                         {/* <motion.p variants={(heroTransition(1))} initial="hidden" animate="visible" className="my-2 text-neutral-900 traking-tighter py-10 max-w-lg dark:text-neutral-300">{HERO_CONTENT}</motion.p> */}
                     </div>
                 </div>           
-                {/* <div className="w-full lg:w-1/2 lg:p-8">
+                <div className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex items-center justify-center">
-                        <motion.img initial={{ x: 100,opacity: 0 }} animate={{ x:0, opacity: 1 }} transition={{duration:1, delay:1.2}}  src={heroImage} alt="Abhishek Sadhwani" />
+                        <motion.img className="w-64 h-64 md:w-96 md:h-96 object-cover mx-auto rounded-full" initial={{ x: 100,opacity: 0 }} animate={{ x:0, opacity: 1 }} transition={{duration:1, delay:1.2}}  src={heroImage} alt="Abhishek Sadhwani" />
                     </div>
-                </div> */}
-            {/* </div> */}
+                 </div>
+            </div>
         </section>
     )
 };
